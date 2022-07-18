f60eb376c843978e541592a0cff0e54712fdec74
bakermd86 <bakermd86@gmail.com>
2022-07-18 19:56:27 -0400

    s01e05 - The Dark of Night (part 2)

--- FGLogClean.py
+++ FGLogClean.py
@@ -215,6 +215,6 @@ def delete_old_log():
 
 if __name__ == '__main__':
     backup_log()
-    formatter = ChatFormatter(CAMPAIGN_DIR, "The Dark of Night (Part 1)", "s01_e05_the_dark_of_night_1")
+    formatter = ChatFormatter(CAMPAIGN_DIR, "The Dark of Night (Part 2)", "s01_e05_the_dark_of_night_2")
     formatter.parse_chatlog()
     delete_old_log()
