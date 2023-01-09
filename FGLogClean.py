2fe05a99eccc5285f7c3843ba85c77a0c4ebd25f
bakermd86 <bakermd6@gmail.com>
2023-01-09 20:50:05 +0100

    s01e06 - High Ground

--- FGLogClean.py
+++ FGLogClean.py
@@ -215,6 +215,6 @@ def delete_old_log():
 
 if __name__ == '__main__':
     backup_log()
-    formatter = ChatFormatter(CAMPAIGN_DIR, "The Dark of Night (Part 8)", "s01_e05_the_dark_of_night_8")
+    formatter = ChatFormatter(CAMPAIGN_DIR, "High Ground (Part 1)", "s01_e06_high_ground_1")
     formatter.parse_chatlog()
     delete_old_log()
