ccb7abd1d76cdc78533f5104d259eba0c1c0dc11
bakermd86 <bakermd6@gmail.com>
2023-08-08 20:32:04 +0200

    S01E09 - 11 & 10s 1011 (Part 2)

--- FGLogClean.py
+++ FGLogClean.py
@@ -215,6 +215,6 @@ def delete_old_log():
 
 if __name__ == '__main__':
     backup_log()
-    formatter = ChatFormatter(CAMPAIGN_DIR, "11&10s 1011 (Part 1)", "s01_e09_1110s1011_1")
+    formatter = ChatFormatter(CAMPAIGN_DIR, "11&10s 1011 (Part 2)", "s01_e09_1110s1011_2")
     formatter.parse_chatlog()
     delete_old_log()
