12088a0d6ff627e3e5494ab21668a3d5586370b3
bakermd86 <bakermd6@gmail.com>
2023-12-08 11:05:08 +0100

    S01E10 - Children of the beast (Part 4)

--- FGLogClean.py
+++ FGLogClean.py
@@ -215,6 +215,6 @@ def delete_old_log():
 
 if __name__ == '__main__':
     backup_log()
-    formatter = ChatFormatter(CAMPAIGN_DIR, "Children of the Beast that Dodged the Abortion (Part 3)", "s01_e10_children_beast_3")
+    formatter = ChatFormatter(CAMPAIGN_DIR, "Children of the Beast that Dodged the Abortion (Part 4)", "s01_e10_children_beast_4")
     formatter.parse_chatlog()
     delete_old_log()
