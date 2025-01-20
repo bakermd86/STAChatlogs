7f3ba4ec9950febf318bbbe5411fe9a8a6725719
bakermd86 <bakermd6@gmail.com>
2025-01-20 20:30:14 +0100

    S02E02 - His Dielectric Materials (Part 6)

--- FGLogClean.py
+++ FGLogClean.py
@@ -294,7 +294,7 @@ def delete_old_log():
 
 if __name__ == '__main__':
     backup_log()
-    formatter = ChatFormatter(CAMPAIGN_DIR, "His Dielectric Materials (Part 5)", "s02_e02_dielectric_5")
+    formatter = ChatFormatter(CAMPAIGN_DIR, "His Dielectric Materials (Part 6)", "s02_e02_dielectric_6")
     formatter.parse_identities()
     formatter.parse_chatlog()
     delete_old_log()
