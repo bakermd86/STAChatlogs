4b5ba6d294a442e870643b7f2c3770d105938b9a
bakermd86 <bakermd6@gmail.com>
2025-03-03 20:41:51 +0100

    S02E03 - Knight Takes Red Queen's Rook, Pawn Defends (Part 5)

--- FGLogClean.py
+++ FGLogClean.py
@@ -294,7 +294,7 @@ def delete_old_log():
 
 if __name__ == '__main__':
     backup_log()
-    formatter = ChatFormatter(CAMPAIGN_DIR, "Knight Takes Red Queen's Rook, Pawn Defends (Part 4)", "s02_e03_knight_takes_rook_4")
+    formatter = ChatFormatter(CAMPAIGN_DIR, "Knight Takes Red Queen's Rook, Pawn Defends (Part 5)", "s02_e03_knight_takes_rook_5")
     formatter.parse_identities()
     formatter.parse_chatlog()
     delete_old_log()
