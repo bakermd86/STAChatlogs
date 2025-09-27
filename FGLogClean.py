700b202b9e3edc8be7f66d385c6cfae8c002147c
bakermd86 <bakermd6@gmail.com>
2025-09-27 21:09:38 +0200

    S02E05 - Corpus Reus (Part 5)

--- FGLogClean.py
+++ FGLogClean.py
@@ -295,7 +295,7 @@ def delete_old_log():
 
 if __name__ == '__main__':
     backup_log()
-    formatter = ChatFormatter(CAMPAIGN_DIR, "Corpus Reus (Part 4)", "s02_e05_corpus_reus_4")
+    formatter = ChatFormatter(CAMPAIGN_DIR, "Corpus Reus (Part 5)", "s02_e05_corpus_reus_5")
     formatter.parse_identities()
     formatter.parse_chatlog()
     delete_old_log()
