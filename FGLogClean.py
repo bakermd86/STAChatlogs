36c617eb52f0b3bc34d31cc4731a1f3a7e2e2f4c
bakermd86 <bakermd86@gmail.com>
2025-10-20 16:53:31 +0200

    S02E06 - Articles of Faith (Part 2)

--- FGLogClean.py
+++ FGLogClean.py
@@ -295,7 +295,7 @@ def delete_old_log():
 
 if __name__ == '__main__':
     backup_log()
-    formatter = ChatFormatter(CAMPAIGN_DIR, "Articles of Faith (Part 1)", "s02_e06_articles_of_faith_1")
+    formatter = ChatFormatter(CAMPAIGN_DIR, "Articles of Faith (Part 2)", "s02_e06_articles_of_faith_2")
     formatter.parse_identities()
     formatter.parse_chatlog()
     delete_old_log()
