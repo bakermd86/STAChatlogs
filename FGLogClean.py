823f85d18e534d54b601a057698a303ab6013945
bakermd86 <bakermd86@gmail.com>
2026-05-04 21:30:34 +0200

    S02E07 - Where the Wild Things Are (Part 4)

--- FGLogClean.py
+++ FGLogClean.py
@@ -295,7 +295,7 @@ def delete_old_log():
 
 if __name__ == '__main__':
     backup_log()
-    formatter = ChatFormatter(CAMPAIGN_DIR, "Where the Wild Things Are (Part 3)", "s02_e07_wild_things_3")
+    formatter = ChatFormatter(CAMPAIGN_DIR, "Where the Wild Things Are (Part 4)", "s02_e07_wild_things_4")
     formatter.parse_identities()
     formatter.parse_chatlog()
     delete_old_log()
